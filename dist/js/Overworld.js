9c318b92eafbaf66d66596af93ced43027a5740c
Nailson Chagas <chagasnailson@gmail.com>
2022-07-25 15:34:24 -0300

    episode 4

--- dist/js/Overworld.js
+++ dist/js/Overworld.js
@@ -3,28 +3,36 @@ class Overworld {
         this.element = config.element;
         this.canvas = this.element.querySelector('.game-canvas');
         this.ctx = this.canvas.getContext('2d');
+        this.map = null;
+    }
+
+    startGameLoop() {
+        const step = () => {
+            this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height); //limpando o frame anterior
+
+            this.map.drawLowerImage(this.ctx); //desenha a parte de baixo do mapa
+
+            Object.values(this.map.gameObjects).forEach(object => { //desenha todos os objetos e personagens do mapa
+                object.update({
+                    arrow: this.directionInput.direction,
+                });
+                object.sprite.draw(this.ctx);
+            });
+
+            this.map.drawUpperImage(this.ctx);//desenha a parte de cima do mapa
+
+            requestAnimationFrame(() => {
+                step(); //roda a cada novo frame
+            });
+        }   
+        step();
     }
 
     init() {
-        const image = new Image();
-        image.src = '../images/maps/DemoLower.png'
-        image.onload = () => {
-            this.ctx.drawImage(image, 0, 0);
-        }
-
-        const hero = new GameObject({
-            x: 5, y: 6
-        })
-
-        const npc1 = new GameObject({
-            x: 4, y: 7,
-            src: '../images/characters/people/npc1.png'
-        })
-
-        setTimeout(() => {
-            hero.sprite.draw(this.ctx);
-            npc1.sprite.draw(this.ctx);
-        }, 200)
-
+        this.map = new OverworldMap(window.OverworldMaps.DemoRoom);
+        this.directionInput = new DirectionInput();
+        this.directionInput.init();
+        
+        this.startGameLoop();
     }
 }
